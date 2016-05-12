6445440f560e31957684a51c31298e84fd71418f
yzpn00 <yzpn00@gmail.com>
2016-05-12 23:27:10 +0900

    更新：血液型チャート追加（仮データ当てはめ）

--- 7/js/app.js
+++ 7/js/app.js
@@ -1,8 +1,27 @@
-//チャート用データ
+<?php
+    //言語設定、内部エンコーディングを指定する
+    mb_language("japanese");
+    mb_internal_encoding("UTF-8");
+    mb_http_output("UTF-8");
+
+    //2. DB接続します
+    $pdo = new PDO('mysql:dbname=an;charset=utf8;host=localhost', 'root', 'root');
+
+    //２．データ登録SQL作成
+    $stmt_count_blood_a = $pdo->prepare("select count(blood) from an_table where bloo like 'A';");
+
+    //３．SQL実行
+    $flag_count_blood_a = $stmt->execute();
+
+    $result_count_blood_a = $stmt->fetch(PDO::FETCH_ASSOC);
+?>
+
+var count_blood_a = <?= $flag_count_blood_a ?>
+    //チャート用データ
 var dataPlot = [
     {
-        label: "リンゴ",
-        y: 10
+        label: "A",
+        y: count_blood_a
     },
     {
         label: "オレンジ",
@@ -24,7 +43,7 @@ var dataPlot = [
 //========================================
 
 //チャートの生成
-var chart = new CanvasJS.Chart("chartContainer_age", {
+var chart = new CanvasJS.Chart("chartContainer_blood", {
     data: [{
         type: 'column',
         dataPoints: dataPlot
--- 7/select.php
+++ 7/select.php
@@ -28,23 +28,71 @@
             <button><a href="index.html">ホームへ</a></button>
         </div>
 
-       <h1>星座</h1>
-        <div id="chartContainer_age"></div>
+        <div id="chartContainer_blood"></div>
+
         <!-- CanvasJS -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/canvasjs/1.7.0/canvasjs.min.js"></script>
-        <script src="./js/app.js"></script>
+        <!--<script src="./js/app.js"></script>-->
+        <script>
+            window.onload = function () {
 
+                /*===== 血液型チャート =====*/
+                var count_blood_a = 20;
+                var count_blood_b = 5;
+                var count_blood_ab = 7;
+                var count_blood_o = 15;
+                var count_blood_other = 3;
+                var count_blood_unknown = 1;
 
+                var chart_blood = new CanvasJS.Chart("chartContainer_blood", {
+                    title: {
+                        text: "血液型チャート"
+                    },
+                    legend: {
+                        maxWidth: 350,
+                        itemWidth: 120
+                    },
+                    data: [
+                        {
+                            type: "pie",
+                            showInLegend: true,
+                            legendText: "{indexLabel}",
+                            dataPoints: [
+                                {
+                                    y: count_blood_a,
+                                    indexLabel: "A"
+                                },
+                                {
+                                    y: count_blood_b,
+                                    indexLabel: "B"
+                                },
+                                {
+                                    y: count_blood_ab,
+                                    indexLabel: "AB"
+                                },
+                                {
+                                    y: count_blood_o,
+                                    indexLabel: "O"
+                                },
+                                {
+                                    y: count_blood_other,
+                                    indexLabel: "その他"
+                                },
+                                {
+                                    y: count_blood_unknown,
+                                    indexLabel: "不明"
+                                }
+                            ]
+                        }
+                    ]
+                });
+                chart_blood.render();
+                /*=========================*/
+            }
+        </script>
     </main>
 
     <footer>
-        <div>
-            <ul>
-                <li>
-
-                </li>
-            </ul>
-        </div>
         <h4>Copyright 2016</h4>
     </footer>
 
